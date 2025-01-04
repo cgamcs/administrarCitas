982f700b8cff5710678ff9d655d85b019cc32c64
César García <cesar.garciamcs@outlook.com>
2025-01-03 20:26:29 -0600

    creando clase para mostrar notificacion

--- js/app.js
+++ js/app.js
@@ -33,9 +33,36 @@ function submitCita(e) {
     e.preventDefault()
 
     if( Object.values(citaObj).some(valor => valor.trim() === '') ) {
-        console.log('Todos los campos son obligatorios')
+        new Notificacion({
+            texto: 'Todos los campos son obligatorios',
+            tipo: 'error'
+        })
         return
     }
 
-    console.log('Enviando formulario')
+}
+
+class Notificacion {
+
+    constructor( {texto, tipo} ) {
+        this.texto = texto
+        this.tipo = tipo
+
+        this.mostrar()
+    }
+
+    mostrar() {
+        // Crear la notificacion
+        const alerta = document.createElement('DIV')
+        alerta.classList.add('text-center', 'w-full', 'p-3', 'text-white', 'my-5', 'alert', 'uppercase', 'font-bold', 'text-sm')
+
+        // Si es de tipo error, agregar una clase
+        this.tipo === 'error' ? alerta.classList.add('bg-red-500') : alerta.classList.add('bg-green-500')
+
+        // Mensaje de alerta
+        alerta.textContent = this.texto
+
+        // Insertar en el DOM
+        formulario.parentElement.insertBefore(alerta, formulario)
+    }
 }

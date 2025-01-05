bdc70cc4f952be61b8c244dcba04a8030b6e8d05
César García <cesar.garciamcs@outlook.com>
2025-01-04 21:01:06 -0600

    primeros pasos mostrando la cita

--- js/app.js
+++ js/app.js
@@ -7,6 +7,8 @@ const sintomasInput = document.querySelector('#sintomas')
 
 const formulario = document.querySelector('#formulario-cita')
 
+const contenedorCitas = document.querySelector('#citas')
+
 // Objeto de cita
 const citaObj = {
     paciente: '',
@@ -67,8 +69,29 @@ class AdminCitas {
 
     agregar(cita) {
         this.citas = [...this.citas, cita]
+        this.mostrar()
+    }
 
-        console.log(this.citas)
+    mostrar() {
+        // Limpiar HTML
+        while(contenedorCitas.firstChild) {
+            contenedorCitas.removeChild(contenedorCitas.firstChild)
+        }
+
+        // Generando las citas
+        this.citas.forEach( cita => {
+            const divCita = document.createElement('DIV')
+            divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl')
+
+            const paciente = document.createElement('P')
+            paciente.classList.add('font-normal', 'mb-3', 'text-gray-700', 'normal-case')
+            paciente.innerHTML = `<span class="font-bold uppercase">Paciente: </span> ${cita.paciente}`
+
+            // Mostrando en el HTML
+            divCita.appendChild(paciente)
+
+            contenedorCitas.appendChild(divCita)
+        })
     }
 }
 

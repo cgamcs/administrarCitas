49a79b24649a37ad17107ee702eb540dc489ba4f
César García <cesar.garciamcs@outlook.com>
2025-01-12 18:30:39 -0600

    eliminando desde IndexedDB

--- js/classes/AdminCitas.js
+++ js/classes/AdminCitas.js
@@ -1,6 +1,7 @@
 import { contenedorCitas } from '../selectores.js'
 import { contenedorDB } from '../variables.js'
 import { cargarEdicion } from "../funciones.js"
+import Notificacion from './Notificacion.js'
 
 export default class AdminCitas {
     
@@ -20,8 +21,21 @@ export default class AdminCitas {
     }
 
     eliminar(id) {
-        this.citas = this.citas.filter( cita => cita.id !== id )
-        this.mostrar()
+        const transaction = contenedorDB.DB.transaction(['citas'], 'readwrite')
+        const objectStore = transaction.objectStore('citas')
+        objectStore.delete(id)
+
+        transaction.oncomplete = () => {
+            new Notificacion({
+                texto: 'Paciente eliminado',
+                tipo: 'error'
+            })
+            this.mostrar()
+        }
+
+        transaction.onerror = () => {
+            console.log('Hubo un error')
+        }
     }
 
     mostrar() {
@@ -37,7 +51,7 @@ export default class AdminCitas {
         const objectStore = contenedorDB.DB.transaction('citas').objectStore('citas')
 
         const total = objectStore.count()
-        total.onsuccess = function() {
+        total.onsuccess = () => {
             const totalCitas = total.result
             
             // Si no hay citas
@@ -46,11 +60,11 @@ export default class AdminCitas {
                 return
             }
 
-            objectStore.openCursor().onsuccess = function(e) {
+            objectStore.openCursor().onsuccess = (e) => {
                 const cursor = e.target.result
     
                 if(cursor) {
-                    const { paciente, propietario, email, fecha, sintomas } = cursor.value
+                    const { paciente, propietario, email, fecha, sintomas, id } = cursor.value
     
                     const divCita = document.createElement('DIV')
                     divCita.classList.add('mx-5', 'my-10', 'bg-white', 'shadow-md', 'px-5', 'py-10', 'rounded-xl')
@@ -81,16 +95,14 @@ export default class AdminCitas {
                     btnEditar.innerHTML = 'Editar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
     
                     // Escuchar el btnEditar y tomar una copia de cita
-                    const cita = cursor.value
-
-                    const clone = structuredClone(cita)
+                    const clone = structuredClone(cursor.value)
                     btnEditar.onclick = () => cargarEdicion(clone)
     
                     const btnEliminar = document.createElement('button');
                     btnEliminar.classList.add('py-2', 'px-10', 'bg-red-600', 'hover:bg-red-700', 'text-white', 'font-bold', 'uppercase', 'rounded-lg', 'flex', 'items-center', 'gap-2');
                     btnEliminar.innerHTML = 'Eliminar <svg fill="none" class="h-5 w-5" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
     
-                    btnEliminar.onclick = () => this.eliminar(cita.id)
+                    btnEliminar.onclick = () => this.eliminar(id)
     
                     const contenedorBotones = document.createElement('DIV')
                     contenedorBotones.classList.add('flex', 'justify-between', 'mt-10')
